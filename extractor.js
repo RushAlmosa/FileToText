987035c074a708ee77136be1bc407f21cbdb4cb7
Rush Almosa <Rush_lk@hotmail.de>
2023-11-17 18:29:45 +0100

    add new design and funktions

--- extractor.js
+++ extractor.js
@@ -1,6 +1,5 @@
-document.getElementById('extractButton').addEventListener('click', function() {
-    var fileInput = document.getElementById('zipFileInput');
-    var file = fileInput.files[0];
+document.getElementById('zipFileInput').addEventListener('change', function(event) {
+    var file = event.target.files[0];
     if (file) {
         extractZip(file);
     }
@@ -9,12 +8,47 @@ document.getElementById('extractButton').addEventListener('click', function() {
 function extractZip(file) {
     JSZip.loadAsync(file).then(function(zip) {
         var output = document.getElementById('output');
-        output.textContent = '';
+        var totalFiles = 0;
+        var totalCharacters = 0;
+        output.textContent = ''; // Clear previous output
 
         zip.forEach(function(relativePath, zipEntry) {
+            totalFiles++;
             zipEntry.async("string").then(function(content) {
+                totalCharacters += content.length;
                 output.textContent += `Path: ${relativePath}\nContent:\n${content}\n\n`;
+
+                // Update statistics after processing each file
+                document.getElementById('fileCount').textContent = `Total Files: ${totalFiles}`;
+                document.getElementById('charCount').textContent = `Total Characters: ${totalCharacters}`;
             });
         });
     });
 }
+
+function downloadContent() {
+    var element = document.createElement('a');
+    var content = document.getElementById('output').textContent;
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+    element.setAttribute('download', 'extracted_content.txt');
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+document.getElementById('downloadButton').addEventListener('click', downloadContent);
+
+function copyContent() {
+    var content = document.getElementById('output').textContent;
+    navigator.clipboard.writeText(content).then(function() {
+        alert('Content copied to clipboard!');
+    }, function(err) {
+        alert('Error in copying text: ', err);
+    });
+}
+
+document.getElementById('copyButton').addEventListener('click', copyContent);
